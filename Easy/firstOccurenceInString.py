6563e83cff1cc8e3d2c4652ee6ad08c7686b1617
iamahmadpk <iamahmadpk444@gmail.com>
2023-07-11 12:59:47 +0500

    index of first occurence in string problem added

---
+++ Easy/firstOccurenceInString.py
@@ -0,0 +1,13 @@
+class Solution:
+	def strStr(self, haystack: str , needle: str) -> int:
+		if needle == "":
+			return 0
+		for i in range(len(haystack) + 1 -len(needle)):
+			if haystack[i:i+len(needle)] == needle:
+				return i
+		return -1
+sol = Solution()
+haystack = "sadbutsad"
+needle = "but"
+result = sol.strStr(haystack,needle)
+print(result)
